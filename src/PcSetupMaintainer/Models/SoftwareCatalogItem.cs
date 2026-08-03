2391df5846b26fa366356a137cb0e01e1938a733
fn4bp69rfz-cpu <fn4bp69rfz@privaterelay.appleid.com>
2026-08-03 14:04:00 +0300

    Update src/PcSetupMaintainer/Models/SoftwareCatalogItem.cs

---
+++ src/PcSetupMaintainer/Models/SoftwareCatalogItem.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PcSetupMaintainer.Models;
+
+public sealed class SoftwareCatalogItem : INotifyPropertyChanged
+{
+    private bool _isSelected;
+
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public required string Category { get; init; }
+    public string Description { get; init; } = "";
+    public string Tags { get; init; } = "";
+
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set
+        {
+            if (_isSelected == value) return;
+            _isSelected = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
